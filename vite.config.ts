4e32d6946ab3cef41f9e8b3c10d6d05310cd65a7
notjitin-1994 <jitin@smartslate.io>
2025-08-24 14:36:32 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    assetsInclude: ['**/*.html'],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
